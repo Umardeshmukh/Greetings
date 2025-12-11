05f6127202ce955aa5300c5b11ece11efb14b019
Umar Deshmukh <umardesh@gmail.com>
2025-12-11 13:43:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var addBtn = document.querySelector('#revealBtn')
+var stopBtn = document.querySelector('#stpbtn')
+var main = document.querySelector('main')
+var nameInput = document.querySelector('#nameInput')
+
+var messageInput = document.querySelector('#messageInput')
+var intervalId = null;
+
+// Start button - begins generating divs
+addBtn.addEventListener('click', function(){
+    if(intervalId) return;
+    
+    intervalId = setInterval(function(){
+        let div = document.createElement('div')
+        var c1 = Math.floor(Math.random()*256);
+        var c2 = Math.floor(Math.random()*256);
+        var c3 = Math.floor(Math.random()*256);
+        var x = Math.random()*100; 
+        var y = Math.random()*100;
+        
+        // Get input values
+        var textToShow = Math.random() > 0.5 ? nameInput.value : messageInput.value;
+        
+        div.textContent = textToShow; // Add text instead of circle
+        div.style.color = `rgb(${c1},${c2},${c3})`
+        div.style.position = 'absolute'
+        div.style.top = y + '%'
+        div.style.left = x + '%'
+        div.style.fontWeight = 'bold'
+        div.style.fontSize = '20px'
+        div.style.whiteSpace = 'nowrap'
+        
+        main.appendChild(div)
+    }, 20); 
+    
+});
+
+// Stop button - stops generating divs
+stopBtn.addEventListener('click', function(){
+    clearInterval(intervalId);
+    intervalId = null;
+});
+
