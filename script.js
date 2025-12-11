5f6f720104ad9649eb178a9551a226fab68189d2
Umros11 <118596403+Umardeshmukh@users.noreply.github.com>
2025-12-11 13:51:43 +0530

    Adjust random div properties and styles
    changed the colors and font size

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ addBtn.addEventListener('click', function(){
     
     intervalId = setInterval(function(){
         let div = document.createElement('div')
-        var c1 = Math.floor(Math.random()*256);
-        var c2 = Math.floor(Math.random()*256);
-        var c3 = Math.floor(Math.random()*256);
-        var x = Math.random()*100; 
-        var y = Math.random()*100;
+        // var c1 = Math.floor(Math.random()*256);
+        // var c2 = Math.floor(Math.random()*256);
+        // var c3 = Math.floor(Math.random()*256);
+        var x = Math.random()*90; 
+        var y = Math.random()*90;
         
         // Get input values
         var textToShow = Math.random() > 0.5 ? nameInput.value : messageInput.value;
         
         div.textContent = textToShow; // Add text instead of circle
-        div.style.color = `rgb(${c1},${c2},${c3})`
+        // div.style.color = `rgb(${c1},${c2},${c3})`
+         div.style.color = "white"
         div.style.position = 'absolute'
         div.style.top = y + '%'
         div.style.left = x + '%'
         div.style.fontWeight = 'bold'
-        div.style.fontSize = '20px'
+        div.style.fontSize = '50px'
         div.style.whiteSpace = 'nowrap'
         
         main.appendChild(div)
